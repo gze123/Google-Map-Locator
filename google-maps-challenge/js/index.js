438fb22ed92655aed521fa5812bbbc24b0cd7be1
Ziy En <gze.2315@gmail.com>
2020-04-19 17:06:17 +0800

    added search by zip code function

--- google-maps-challenge/js/index.js
+++ google-maps-challenge/js/index.js
@@ -1,5 +1,5 @@
 window.onload = () => {
-  displayStores();
+
 };
 
 var map;
@@ -7,74 +7,136 @@ var markers = [];
 var infoWindow;
 
 function initMap() {
-  var losAngelas = {
-    lat: 34.06338,
-    lng: -118.35808
-  };
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: losAngelas,
-    zoom: 11,
-    mapTypeId: "roadmap"
-  });
-  infoWindow = new google.maps.InfoWindow();
-  showStoresMarkers();
+    var losAngelas = {
+        lat: 34.06338,
+        lng: -118.35808
+    };
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: losAngelas,
+        zoom: 11,
+        mapTypeId: "roadmap"
+    });
+    infoWindow = new google.maps.InfoWindow();
+    searchStores();
 }
 
-function displayStores() {
-  var storesHtml = "";
-  for (var [index, store] of stores.entries()) {
-    storesHtml += `
+function searchStores() {
+  var foundStores = [];
+  var zipCode = document.getElementById('zip-code-input').value;
+  if(zipCode) {
+    for (var store of stores) {
+      var postal = store.address.postalCode.substring(0,5);
+      if(postal == zipCode) {
+        foundStores.push(store);
+      }
+    }
+  } else {
+    foundStores = stores;
+  }
+  clearLocations();
+  displayStores(foundStores);
+  showStoresMarkers(foundStores);
+  setOnClickListener();
+}
+
+function clearLocations() {
+  infoWindow.close();
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(null);
+  }
+  markers.length = 0;
+
+}
+
+function setOnClickListener() {
+  var storeElements = document.querySelectorAll('.store-container');
+  storeElements.forEach(function (elem, index) {
+    elem.addEventListener('click', function () {
+      new google.maps.event.trigger(markers[index], 'click')
+    })
+  })
+}
+
+function displayStores(stores) {
+    var storesHtml = "";
+    for (var [index, store] of stores.entries()) {
+        storesHtml += `
       <div class="store-container">
-          <div class="store-info-container">
-            <div class="store-address">
-              <span>
-                ${store.addressLines[0]}
-              </span>
-              <span>
-                ${store.addressLines[1]}
-              </span>
+        <div class="store-container-background">
+            <div class="store-info-container">
+                <div class="store-address">
+                <span>
+                    ${store.addressLines[0]}
+                </span>
+                <span>
+                    ${store.addressLines[1]}
+                </span>
+                </div>
+                <div class="store-phone-number">
+                    ${store.phoneNumber}
+                </div>
             </div>
-            <div class="store-phone-number">
-                ${store.phoneNumber}
-            </div>
-          </div>
 
-          <div class="store-number-container">
-            <div class="store-number">
-              ${++index}
+            <div class="store-number-container">
+                <div class="store-number">
+                ${++index}
+                </div>
             </div>
-          </div>
         </div>
+      </div>
       `;
-    document.querySelector(".stores-list").innerHTML = storesHtml;
-  }
+        document.querySelector(".stores-list").innerHTML = storesHtml;
+    }
 }
 
-function showStoresMarkers() {
-  var bounds = new google.maps.LatLngBounds();
-  for (var [index, store] of stores.entries()) {
-    var latlng = new google.maps.LatLng(
-      store.coordinates.latitude,
-      store.coordinates.longitude
-    );
-    var name = store.name;
-    var address = store.addressLines[0];
-    bounds.extend(latlng);
-    createMarker(latlng, name, address, index + 1);
-  }
-  map.fitBounds(bounds);
+function showStoresMarkers(stores) {
+    var bounds = new google.maps.LatLngBounds();
+    for (var [index, store] of stores.entries()) {
+        var latlng = new google.maps.LatLng(
+            store.coordinates.latitude,
+            store.coordinates.longitude
+        );
+        var name = store.name;
+        var address = store.addressLines[0];
+        var openStatusText = store.openStatusText;
+        var phoneNumber = store.phoneNumber;
+        bounds.extend(latlng);
+        createMarker(latlng, name, address, openStatusText, phoneNumber, index + 1);
+    }
+    map.fitBounds(bounds);
 }
 
-function createMarker(latlng, name, address, index) {
-  var html = "<b>" + name + "</b> <br/>" + address;
-  var marker = new google.maps.Marker({
-    map: map,
-    position: latlng,
-    label: index.toString()
-  });
-  google.maps.event.addListener(marker, "click", function() {
-    infoWindow.setContent(html);
-    infoWindow.open(map, marker);
-  });
-  markers.push(marker);
+function createMarker(latlng, name, address, openStatusText, phoneNumber, index) {
+    var html = `
+    <div class="store-info-window">
+        <div class="store-info-name">
+            ${name}
+        </div>
+        <div class="store-info-status">
+            ${openStatusText}
+        </div>
+        <div class="store-info-address">
+            <div class="circle">
+                <i class="fas fa-location-arrow"></i>
+            </div>
+            ${address}
+        </div>
+        <div class="store-info-phone">
+            <div class="circle">
+                <i class="fas fa-phone"></i>
+            </div>
+            ${phoneNumber}
+        </div>
+    </div>
+  `;
+    var marker = new google.maps.Marker({
+        map: map,
+        position: latlng,
+        label: index.toString()
+    });
+    google.maps.event.addListener(marker, "click", function () {
+        infoWindow.setContent(html);
+        infoWindow.open(map, marker);
+    });
+    markers.push(marker);
 }
